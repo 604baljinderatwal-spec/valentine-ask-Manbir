c96b24f6857f0ce922c028abc1439e68ddf1877f
604baljinderatwal-spec <604baljinderatwal@gmail.com>
2026-02-09 03:05:40 -0800

    Change title text on yes button click

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ noBtn.addEventListener("mouseover", () => {
 // YES is clicked
 
 yesBtn.addEventListener("click", () => {
-    title.textContent = "Yippeeee!";
+    title.textContent = "I love you too Manbir!";
 
     catImg.src = "cat_dance.gif";
 
